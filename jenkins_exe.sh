6f9309975ab36ea68001b4c3fdf75a3d8f45e75c
imran21 <mohamedimran31@gmail.com>
2018-04-23 16:37:29 +0530

    Update jenkins_exe.sh

--- jenkins_exe.sh
+++ jenkins_exe.sh
@@ -1,13 +1,8 @@
 #! /bin/bash
 
-<<<<<<< HEAD
+
 cd /root/git_jenkins_work
 ls
 javac Hello.java
 java Hello
 
-=======
-ls
-javac Hello.java
-java Hello
->>>>>>> acffdb0e0763910291db3b4874f7eb0d184d5b13
